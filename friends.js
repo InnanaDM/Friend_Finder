12aa12dc8acb14869da8d6e6c579922292c6e1d8
IDM.air <innanadm8@gmail.com>
2019-05-29 00:14:34 -0400

    array of users

---
+++ friends.js
@@ -0,0 +1,116 @@
+var friends = [{
+    "name": "Haile",
+    "photo": 'https://i.ytimg.com/vi/Ce7hJ24a8yM/maxresdefault.jpghttp://adst.org/wp-content/uploads/2015/10/haile_selassie_interview_with_editor_of_the_voice_of_ethiopia_april_05_1948_-e1444415349633.jpg',
+    "scores": [
+        5,
+        1,
+        1,
+        5,
+        5,
+        1,
+        5,
+        1,
+        5,
+        5
+    ]
+},
+{
+    "name": "bell hooks",
+    "photo": 'https://pbs.twimg.com/profile_images/965350964545847297/4mNK1-tk.jpg',
+    "scores": [
+        2,
+        4,
+        5,
+        2,
+        3,
+        1,
+        3,
+        5,
+        1,
+        4
+    ]
+},
+{
+    "name": "Louis",
+    "photo": 'https://spartacus-educational.com/USAfarrakhan.jpg',
+    "scores": [
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2,
+        3,
+        2
+    ]
+},
+{
+    "name": "Dick",
+    "photo": 'https://2.bp.blogspot.com/-A1eOr5NEtAA/WZ9eEVySCdI/AAAAAAAER5I/Fy16W6PqRHsRkNiJJjPoynVm8i0vnjf5wCLcBGAs/s1600/Dick%2BGregory.jpg',
+    "scores": [
+        2,
+        2,
+        2,
+        3,
+        4,
+        3,
+        4,
+        5,
+        1,
+        1
+    ]
+},
+{
+    "name": "Sebi",
+    "photo": 'http://epmgaa.media.clients.ellingtoncms.com/img/photos/2016/08/09/13900326_1184967124856779_3606339334484318897_n_t580.jpg?8f1b5874916776826eb17d7e67de7278c987ca33',
+    "scores": [
+        1,
+        1,
+        4,
+        4,
+        2,
+        2,
+        4,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Angela",
+    "photo": 'http://info.umkc.edu/womenc/wp-content/uploads/2018/02/z.jpg',
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        3,
+        3,
+        2,
+        2
+    ]
+},
+{
+    "name": "Ella",
+    "photo": 'http://www.greatblackheroes.com/wp-content/uploads/2018/11/ellabaker02.jpg',
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2
+    ]
+}
+]
+
+module.exports = friends;
+
